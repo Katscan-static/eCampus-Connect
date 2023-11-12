77783410cf1a25173352cde0be24bbdefdf2d4c8
Katekani Madingana <madinganakn@gmail.com>
2023-11-12 14:04:45 +0200

    backend and database intergration

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from data_handler import DataHandler
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -6,21 +7,45 @@ app = Flask(__name__, static_url_path='/static')
 def home():
     return render_template('index.html')
     
-@app.route("/login")
+@app.route("/login",methods = ['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        login = request.form.to_dict()
+        login_DH = DataHandler(login)
+        test = login_DH.pw_compare()
+        return test
     return render_template('login.html')
     
-@app.route("/signup")
+@app.route("/signup", methods = ['POST', 'GET'])
 def signup():
+    if request.method == 'POST':
+        signup = request.form.to_dict()
+        signup_DH = DataHandler(signup)
+        if signup_DH.check_email():
+            return render_template('signup.html', message = "User Already Exists Please login")
+        signup_DH.insert()
+        return render_template('signup_confirm.html', signup = signup)
     return render_template('signup.html')
     
-@app.route("/contact")
+@app.route("/contact",methods = ['POST','GET'])
 def contact():
+    if request.method == "POST":
+        contact = request.form
+        print(contact)
+        return render_template('thankyou.html', contact = contact)
+       
     return render_template('contact.html')
 
 @app.route("/aboutus")
 def aboutus():
     return render_template('aboutus.html')
+    
+@app.route("/contact_us",methods = ['POST','GET'])
+def contact_us():
+    if request.method == "POST":
+        contact = request.form
+        print(contact)
+    
 
 if __name__ == "__main__":
     app.run()

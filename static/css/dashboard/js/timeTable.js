8c662286c86de231ede06bc5010dc349795b4989
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-11-15 20:24:01 +0200

    dashbord

---
+++ static/css/dashboard/js/timeTable.js
@@ -0,0 +1,133 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '38-718',
+        subject: 'DBMS130',
+        type: 'Sprint'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '38-718',
+        subject: 'MTH166',
+        type: 'Hackerthorn'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '38-718',
+        subject: 'NS200',
+        type: 'Sprint'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '27-304Y',
+        subject: 'MTH166',
+        type: 'Hackerthorn'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '28-107',
+        subject: 'CS849',
+        type: 'Sprint'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '28-107',
+        subject: 'CS849',
+        type: 'Sprint'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'NS200',
+        type: 'Sprint'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'DBMS130',
+        type: 'Sprint'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '38-719',
+        subject: 'CS200',
+        type: 'Sprint'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'MTH166',
+        type: 'Sprint'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '38-719',
+        subject: 'CS849',
+        type: 'Sprint'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-718',
+        subject: 'NS200',
+        type: 'Sprint'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'MEC103',
+        type: 'Sprint'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '33-309',
+        subject: 'MEC103',
+        type: 'Sprint'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '33-601',
+        subject: 'CS849',
+        type: 'Hackerthorn'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '34-604',
+        subject: 'DBMS130',
+        type: 'Hackerthorn'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '34-604',
+        subject: 'DBMS130',
+        type: 'Sprint'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '33-309',
+        subject: 'MTH166',
+        type: 'Sprint'
+    }
+]
